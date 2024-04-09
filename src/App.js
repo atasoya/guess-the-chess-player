7d32d80aee01edab886500a8f712ef13cc41044a
atasoya <ataatasoy2013@gmail.com>
2024-04-09 10:34:41 +0300

    ui improvement & mobile compability

--- src/App.js
+++ src/App.js
@@ -28,11 +28,18 @@ const titleHierarchy = {
 const getTitleHierarchyValue = (title) => titleHierarchy[title] || 0;
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [randomPlayer, setRandomPlayer] = useState({});
   const [selectedPlayer, setSelectedPlayer] = useState(null);
   const [isWinner, setIsWinner] = useState(false);
   const [selectedPlayerNames, setSelectedPlayerNames] = useState([]);
   const [blurLevel, setBlurLevel] = useState(30);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   useEffect(() => {
     const randomIndex = Math.floor(Math.random() * chessPlayers.length);
@@ -53,17 +60,23 @@ function App() {
     setBlurLevel(30);
   };
 
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter' && selectedPlayer) {
-      if(selectedPlayer===randomPlayer){
+  const handleGuess = () => {
+    if (selectedPlayer) {
+      if(selectedPlayer === randomPlayer){
         setBlurLevel(0);
-        setIsWinner(true)
+        setIsWinner(true);
       }else{
-        setIsWinner(false)
+        setIsWinner(false);
       }
-      setSelectedPlayerNames(prevNames =>[selectedPlayer, ...prevNames]);
+      setSelectedPlayerNames(prevNames => [selectedPlayer, ...prevNames]);
       setSelectedPlayer(null);
       setBlurLevel(prevLevel => Math.max(prevLevel - 5, 0));
+    }
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      handleGuess();
     }
   };
 
@@ -72,7 +85,7 @@ function App() {
       <div className="bg-custom-black h-20 w-screen flex justify-center items-center">
         <p className="text-custom-white font-bold text-lg">Guess The Chess Player</p>
       </div>
-      <div className="coffee-image-container" style={{ position: 'relative', textAlign: 'center', marginTop: '20px',marginLeft: '20px' }}>
+      <div className="coffee-image-container" style={{ position: 'relative', textAlign: 'center', marginTop: '20px',"marginLeft":"20px" }}>
       <a href="https://www.buymeacoffee.com/atasoyata" target="_blank">
         <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style={{ height: '40px', width: '145px' }} />
       </a>
@@ -86,8 +99,9 @@ function App() {
           </div>
         </div>
       )}
-      <div className="flex justify-center mt-10">
-        <div className="flex flex-col items-center bg-custom-white shadow-lg rounded-lg p-5">
+
+      <div className="flex justify-center mt-10"style={{ "marginTop":"10px" }}>
+        <div className="flex flex-col items-center bg-custom-white shadow-lg rounded-lg p-5" >
           <div className="bg-custom-grey h-64 w-64 mb-4 flex items-center justify-center relative">
             <img
               src={randomPlayer.imageUrl || ''}
@@ -100,19 +114,38 @@ function App() {
             disablePortal
             id="combo-box-demo"
             options={chessPlayers}
-            sx={{ width: 300,
-              '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
-                borderColor: '#00ADB5',
-              },
-              '& .MuiInputLabel-root.Mui-focused': {
-                color: 'black',
-              },}}
-            renderInput={(params) => <TextField {...params} label="Guess The Chess Player" onKeyDown={handleKeyDown} />}
+            sx={{ width: 300 }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Guess The Chess Player"
+                onKeyDown={handleKeyDown}
+              />
+            )}
             onChange={handleAutocompleteChange}
             value={selectedPlayer}
           />
+          {windowWidth <= 768 && (
+            <button
+              className="mobile-only-button"
+              onClick={handleGuess} // Use handleGuess for button click
+              style={{
+                display: 'block',
+                marginTop: '10px',
+                backgroundColor: '#00ADB5',
+                color: 'white',
+                border: 'none',
+                padding: '10px 20px',
+                borderRadius: '5px',
+                cursor: 'pointer',
+              }}
+            >
+              Guess
+            </button>
+          )}
         </div>
       </div>
+
       {selectedPlayerNames.length > 0 && (
         <div className="mt-4 px-5">
           <div className="stack">
@@ -125,25 +158,25 @@ function App() {
                       Peak Elo
                       {randomPlayer && player.elo > randomPlayer.elo ? <ArrowDownwardIcon style={{ color: arrowColor }} /> : player.elo < randomPlayer.elo ? <ArrowUpwardIcon style={{ color: arrowColor }} /> : null}
                     </div>
-                    <div className="circle" style={{ backgroundColor: player.elo === randomPlayer.elo ? 'green' : '' }}>{player.elo}</div>
+                    <div className="circle">{player.elo}</div>
                   </div>
                   <div className="circle-container">
                     <div className="circle-text">Nationality</div>
-                    <div className="circle" style={{ backgroundColor: player.nationality === randomPlayer.nationality ? 'green' : '' }}>{player.nationality}</div>
+                    <div className="circle">{player.nationality}</div>
                   </div>
                   <div className="circle-container">
                     <div className="circle-text">
                       Born Year
                       {randomPlayer && player.born > randomPlayer.born ? <ArrowDownwardIcon style={{ color: arrowColor }} /> : player.born < randomPlayer.born ? <ArrowUpwardIcon style={{ color: arrowColor }} /> : null}
                     </div>
-                    <div className="circle" style={{ backgroundColor: player.born === randomPlayer.born ? 'green' : '' }}>{player.born}</div>
+                    <div className="circle">{player.born}</div>
                   </div>
                   <div className="circle-container">
                     <div className="circle-text">
                       Title
                       {randomPlayer && getTitleHierarchyValue(player.title) > getTitleHierarchyValue(randomPlayer.title) ? <ArrowDownwardIcon style={{ color: arrowColor }} /> : getTitleHierarchyValue(player.title) < getTitleHierarchyValue(randomPlayer.title) ? <ArrowUpwardIcon style={{ color: arrowColor }} /> : null}
                     </div>
-                    <div className="circle" style={{ backgroundColor: player.title === randomPlayer.title ? 'green' : '' }}>{player.title}</div>
+                    <div className="circle">{player.title}</div>
                   </div>
                 </div>
               </div>
