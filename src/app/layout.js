9b7d7efca1200d9ccc24753a1bf2d9204395454b
atasoya <ataatasoy2013@gmail.com>
2025-03-06 23:34:49 +0200

    Add Vercel Analytics to track website usage

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import './globals.css';
+import { Analytics } from '@vercel/analytics/next';
+
 
 export const metadata = {
   title: 'Guess The Chess Player',
@@ -8,7 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>{children}
+      <Analytics />
+      </body>
     </html>
   );
 } 
